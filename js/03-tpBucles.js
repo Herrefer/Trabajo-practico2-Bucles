8483ac2df641a64cd8d41aa611f02a60aac60254
Herrera Fernando <herrera770@outlook.es>
2023-11-01 16:19:06 -0300

    se editó el codigo del tp 3

--- js/03-tpBucles.js
+++ js/03-tpBucles.js
@@ -1,5 +1,10 @@
+do {
+  let texto = prompt("Introduzca un texto");
+  document.write(texto + "-");
+} while (confirm("¿Agregar otro texto?"));
 
-do{
-let texto = prompt("Introduzca un texto");
-document.write (texto + "-");
-}while (confirm("¿Agregar otro texto?"));
+// while(confirm("Agregar + texto?")== true){
+//     let texto= prompt("Pone texto aqui");
+//     document.write (texto + "-");
+
+// }
